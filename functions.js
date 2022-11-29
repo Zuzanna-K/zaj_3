e95d35be1aa3ac9c2c4f2a803f76756a9356eaa7
Zuzanna-K <104144931+Zuzanna-K@users.noreply.github.com>
2022-11-29 09:15:04 +0100

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,16 @@
+function isOdd (a)
+{
+    if (a%2==0) return false;
+    return true;
+}
+
+function isOdd2 (a)
+{
+    return (a%2==0);
+}
+
+var isEven =a =>
+{
+    if (a%2==0) return true;
+    return false;
+}
